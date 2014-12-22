d1296b78ec6144527df88f4faf9c3818b5b2a586
sudarsun kannan <sudarsun@gatech.edu>
2014-12-22 13:48:21 -0500

    changes

--- fileshare/filesync.sh
+++ fileshare/filesync.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-rsync sudarsun@hendrix.cc.gt.atl.ga.us:/home/sudarsun/apps_all/datasets/*tar.gz data/
+mkdir ~/shared_data/
+#mkdir ~/shared_data/client_data/
+#export DATAHOST=sudarsun@hendrix.cc.gt.atl.ga.us
+#export DATAHOSTPATH=sudarsun@hendrix.cc.gt.atl.ga.us
+
+echo "make sure to export DATAHOST"
+
+rsync $DATAHOST:~/shared_data ~/shared_data
